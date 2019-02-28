19c0fecd7711d03d46117e3449bff11239da60f6
saherico <47536579+saherico@users.noreply.github.com>
2019-03-01 00:38:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const express = require('express')
 
-const messages = require('./routes/api/messages')
-const notifications = require('./routes/api/notifications')
-const projects = require('./routes/api/Projects')
+const admins = require('./routes/api/admins')
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`
-    <a href="/api/notifications">Notifications</a>
-    <a href="/api/messages">Messages</a>
-    <a href="/api/Projects">Projects</a>
-
+    res.send(`<h1>Welcome to Admin Store</h1>
+    <a href="/api/users">admins</a>
     `);
 })
 
 // Direct routes to appropriate files 
-app.use('/api/messages', messages)
-app.use('/api/notifications', notifications)
-app.use('/api/Projects',projects)
+app.use('/api/admins', admins)
+
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
 
-const port = 3000
+ //port should 3000
+const port = 3001
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
