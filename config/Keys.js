424d8f61acc74f76d0da703327c6c769208a0cb9
MahmouddAhmed <mahmoud.souad@student.guc.edu.eg>
2019-03-22 19:00:58 +0200

    testing

---
+++ config/Keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodb+srv://mahmoud:mahmoud@cluster0-o30ve.mongodb.net/test?retryWrites=true
+}   
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
-
 const express = require('express')
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
+
+// Connect to mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
+
+// Init middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 const consultancyAgencies = require('./routes/api/consultancyAgencies')
@@ -55,12 +66,12 @@ app.use('/api/locations',locations)
 
 
 
-// Handling 404
-//app.use((req, res) => {
-//    res.status(404).send({err: 'We can not find what you are looking for'});
- //})
+ Handling 404
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
 
-const port = 3000
+const port = process.env.PORT ||3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
 
 
