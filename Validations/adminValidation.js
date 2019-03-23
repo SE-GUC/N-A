afefc3d8cf80580698aeff05540b71365cb25d3b
saherico <mohamed.saher@student.guc.edu.eg>
2019-03-23 23:36:28 +0200

    saherico

---
+++ Validations/adminValidation.js
@@ -0,0 +1,28 @@
+const Joi = require('joi')
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            first_name: Joi.string().min(3).max(500).required(),
+            last_name: Joi.string().min(3).max(500).required(),
+            email: Joi.string().min(3).max(500).required(),
+            Password: Joi.string().min(3).max(500).required(),
+            Birthdate: Joi.string().min(3).max(500).required(),
+            JoinDate: Joi.number().min(3).max(50000).required()
+        }
+
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+            first_name: Joi.string().min(3).max(500).required(),
+            last_name: Joi.string().min(3).max(500).required(),
+            email: Joi.string().min(3).max(500).required(),
+            Password: Joi.string().min(3).max(500).required(),
+            Birthdate: Joi.string().min(3).max(500).required(),
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+}
--- routes/api/admins.js
+++ routes/api/admins.js
@@ -1,126 +1,40 @@
 // Dependencies
 const express = require('express');
-const Joi = require('joi');
-const uuid = require('uuid');
 const router = express.Router();
+const mongoose = require('mongoose')
+const admin= require('../../models/admin');
+const validator = require('../../Validations/adminValidation.js')
 
-// Models
-const Admin = require('../../models/Admin');
-
-// temporary data created as if it was pulled out of the database ...
-const admins = [
-	new Admin('Barney','Saeed', 'barney.saed@gmail.com','3afasfa0','October 2,1998'),
-];
-
-// Instead of app use route
-// No need to write the full route
-// res.json() Automatically sends a status of 200
-
-// // Get all admins
-// router.get('/', (req, res) => res.json({ data: admins }));
-
-// Get all admin
-router.get('/', (req, res) => res.json({ data: admins }));
-
-// Create a new admin
-router.post('/', (req, res) => {
-	const first_name = req.body.first_name;
-	const last_name = req.body.last_name;
-	const email = req.body.email;
-	const password = req.body.password;
-	const birth_date = req.body.birth_date;
-
-	if (!first_name) return res.status(400).send({ err: 'First name field is required' });
-	if (typeof first_name !== 'string') return res.status(400).send({ err: 'Invalid value for first name.' });
-	if (!last_name) return res.status(400).send({ err: 'Last name field is required' });
-	if (typeof last_name !== 'string') return res.status(400).send({ err: 'Invalid value for last name.' });
-	// if (!email) return res.status(400).send({ err: 'email field is required' });
-    // if (typeof email !== 'string') return res.status(400).send({ err: 'Invalid value for email' });
-	// if (!password) return res.status(400).send({ err: 'password field is required' });
-	// if (!Birthdate) return res.status(400).send({ err: 'birthdate field is required' });
-
-	// const newAdmin = {
-	// 	first_name,
-	// 	last_name,
-	// 	email,
-	// 	password,
-	// 	birth_date,
-	// };
-
-	const newAdmin = new Admin(first_name, last_name, email, password ,birth_date)
-	admins.push(newAdmin)
-	return res.json({ data: newAdmin });
-});
-
-// router.post('/joi', (req, res) => {
-// 	const name = req.body.name
-// 	const age = req.body.age
-
-// 	const schema = {
-// 		name: Joi.string().min(3).required(),
-// 		age: Joi.number().required(),
-// 	}
-
-// 	const result = Joi.validate(req.body, schema);
-
-// 	if (result.error) return res.status(400).send({ error: result.error.details[0].message });
-
-// 	const newUser = {
-// 		name,
-// 		age,
-// 		id: uuid.v4(),
-// 	};
-// 	return res.json({ data: newUser });
-// });
-
-
-// update a certain Admin
-
-router.put('/:id', (req, res) => {
-
-	const AdminId = req.params.id; 
-
-	const Admin = admins.find(Admin => Admin.id === AdminId)
-
-	if(req.body.first_name)
-
-	Admin.first_name = req.body.first_name;
-
-	if(req.body.last_name)
-
-	Admin.last_name=req.body.last_name;
-
-	if(req.body.password)
-
-	Admin.password=req.body.password;
-
-	if(req.body.Birthdate)
-
-	Admin.birth_date=req.body.birth_date;
-
-	if(req.body.email)
-
-	Admin.email=req.body.email;
-
-    res.send(Admin)
-
+//Get all admins
+router.get('/', async (req,res) => {
+    const admins = await admin.find()
+    res.json({data: admins})
 })
 
-// delete a certain admin
+// Get a certain admin 
+router.get('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    admin.findOneAndUpdate({_id :todoID},{},{returnOriginal :true},(err,result)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(result);
+        }
+      })   });
 
-router.delete('/:id', (req, res) => {
+// Create a admin
+router.post('/', async (req,res) => {
+    try {
+     const isValidated = validator.createValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const newadmin = await admin.create(req.body)
+     res.json({msg:'admin signed up successfully', data: newadmin})
+    }
+    catch(error) {
+        console.log(error)
+    }
+ })
 
-    const AdminId = req.params.id 
-
-    const Admin = admins.find(Admin => Admin.id === 	AdminId)
-
-    const index = admins.indexOf(Admin)
-
-    admins.splice(index,1)
-
-    res.send(admins)
-
-})
-
+ 
 
 module.exports = router;
