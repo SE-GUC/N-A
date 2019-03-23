4b4a7a113213048b542ad1e6babca05214aab6d8
madoooaboelazaiem <33963454+madoooaboelazaiem@users.noreply.github.com>
2019-03-24 01:47:04 +0200

    Update admins.js

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -11,6 +11,31 @@ router.get('/', async (req,res) => {
     res.json({data: admins})
 })
 
+// Get a certain admin 
+router.get('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    admin.findOneAndUpdate({_id :todoID},{},{returnOriginal :true},(err,result)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(result);
+        }
+      })   });
+
+// Create a admin
+router.post('/', async (req,res) => {
+    try {
+     const isValidated = validator.createValidation(req.body)
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+     const newadmin = await admin.create(req.body)
+     res.json({msg:'admin signed up successfully', data: newadmin})
+    }
+    catch(error) {
+        console.log(error)
+    }
+ })
+
+
  // Update a admin's details
 router.put('/:id',(req,res)=>{
     const todoID = req.params.id;
