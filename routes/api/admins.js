866573bf5ffd56411b9df689321aba012d4ee7a5
madoooaboelazaiem <33963454+madoooaboelazaiem@users.noreply.github.com>
2019-03-23 23:57:34 +0200

    You have a proble mmisunderstanding

--- routes/api/admins.js
+++ routes/api/admins.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
-const admin= require('../../models/admin');
+const admin= require('../../models/User.js');
 const validator = require('../../Validations/adminValidation.js')
 
 //Get all admins
