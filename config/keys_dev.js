72b6cfa65e56487f95207c387db89723cb855660
Mahmoudselim5 <m.selim.x5@gmail.com>
2019-03-23 23:32:44 +0200

    adminUpdateDelete_Selim

---
+++ config/keys_dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:"mongodb+srv://mahmoud5598:MAHmoud5598@se-3hs9s.mongodb.net/test",
+}
--- index.js
+++ index.js
@@ -1,59 +1,32 @@
 
 
 const express = require('express')
+const mongoose = require('mongoose')
 
-
-const consultancyAgencies = require('./routes/api/consultancyAgencies')
 const admins = require('./routes/api/admins')
-const Co_working_spaces = require('./routes/api/Co_working_spaces')
-const messages = require('./routes/api/messages')
-const notifications = require('./routes/api/notifications')
-const projects = require('./routes/api/Projects')
-const partner = require('./routes/api/partner')
-const Candidate = require('./routes/api/Candidates')
-const locations = require('./routes/api/locations')
-
-
-
 
 const app = express()
+const db = require('./config/keys').mongoURI
+
+mongoose
+    .connect(db)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
+
+// Init middleware
 app.use(express.json())
-
-app.get('/', (req, res) => {
-
-    res.send(`<h1>Welcome </h1>
-    <a href="/api/consultancyAgencies">consultancyAgencies</a>
-    <a href="/api/Co_working_spaces">Co_working_spaces</a>
-    <a href="/api/admins">admins</a>
-    <a href="/api/partner">partner</a>
-    <a href="/api/notifications">Notifications</a>
-    <a href="/api/messages">Messages</a>
-    <a href="/api/Projects">Projects</a>
-    <a href="/api/Candidates">Candidates</a>
-    <a href="/api/locations">locations</a>
-    
-    
+app.use(express.urlencoded({extended: false}))
 
 
+app.get('/', (req,res) => res.send('<h1>Candidate Store</h1>'))
+app.get('/test', (req,res) => res.send('<h1>Deployed on Heroku</h1>'))
 
-    `);
-})
+// Direct to Route Handler
+//app.use('/api/Candidates',Candidate)
 
 // Direct routes to appropriate files 
 
-app.use('/api/consultancyAgencies', consultancyAgencies)
 app.use('/api/admins', admins)
-app.use('/api/Co_working_spaces', Co_working_spaces)
-app.use('/api/partner', partner)
-app.use('/api/Candidates',Candidate)
-app.use('/api/messages', messages)
-app.use('/api/notifications', notifications)
-app.use('/api/Projects',projects)
-app.use('/api/locations',locations)
-
-
-
-
 
 // Handling 404
 //app.use((req, res) => {
