a382f15615b3f94dc053c1747fcb3ce9d3ef5ee9
MahmouddAhmed <47020019+MahmouddAhmed@users.noreply.github.com>
2019-03-24 02:43:28 +0200

    Update Keys.js

--- config/Keys.js
+++ config/Keys.js
@@ -1,3 +1,4 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://mahmoud:mahmoud@cluster0-o30ve.mongodb.net/test?retryWrites=true'
-}   
+if(process.env.NODE_ENV === 'production') 
+    module.exports = require('./keys_prod')
+else 
+    module.exports = require('./keys_dev')
