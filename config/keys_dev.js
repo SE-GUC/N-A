9179e2f9bc47cc82a62439db197bbe76a530645d
usfshihy <47534492+usfshihy@users.noreply.github.com>
2019-03-28 17:30:36 +0200

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,7 +1,2 @@
-module.exports = {
-<<<<<<< HEAD
-    mongoURI:"mongodb+srv://youssef:1234@cluster0-qdciv.mongodb.net/test"
-}
-=======
-mongoURI: 'mongodb+srv://mahmoud:mahmoud@cluster0-o30ve.mongodb.net/test?retryWrites=true'}
->>>>>>> 98fdd8192b06b0d4daea57bba11540a4aadbb45a
+module.exports = {mongoURI:"mongodb+srv://youssef:1234@cluster0-qdciv.mongodb.net/test"}
+
