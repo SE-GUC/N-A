dadcaea68d84ab27c2fdf76230d8d9803419e0f1
YoussefH1998 <yh_fouad@hotmail.com>
2019-03-28 08:24:26 +0200

    updatedindex.js

--- index.js
+++ index.js
@@ -15,14 +15,7 @@ app.use(express.urlencoded({extended: false}))
 
 
 const consultancyAgencies = require('./routes/api/consultancyAgencies')
-const admins = require('./routes/api/admins')
-const Co_working_spaces = require('./routes/api/Co_working_spaces')
-//const messages = require('./routes/api/messages')
-//const notifications = require('./routes/api/notifications')
-const projects = require('./routes/api/Projects')
-const partner = require('./routes/api/partner')
-const Candidate = require('./routes/api/Candidates')
-const locations = require('./routes/api/locations')
+
 
 
 
@@ -31,13 +24,7 @@ app.get('/', (req, res) => {
 
     res.send(`<h1>Welcome </h1>
     <a href="/api/consultancyAgencies">consultancyAgencies</a>
-    <a href="/api/Co_working_spaces">Co_working_spaces</a>
-    <a href="/api/admins">admins</a>
-    <a href="/api/partner">partner</a>
-    
-    <a href="/api/Projects">Projects</a>
-    <a href="/api/Candidates">Candidates</a>
-    <a href="/api/locations">locations</a>
+
     
     
     `);
@@ -46,14 +33,6 @@ app.get('/', (req, res) => {
 // Direct routes to appropriate files 
 
 app.use('/api/consultancyAgencies', consultancyAgencies)
-app.use('/api/admins', admins)
-app.use('/api/Co_working_spaces', Co_working_spaces)
-app.use('/api/partner', partner)
-app.use('/api/Candidates',Candidate)
-//app.use('/api/messages', messages)
-//app.use('/api/notifications', notifications)
-app.use('/api/Projects',projects)
-app.use('/api/locations',locations)
 
 
 
