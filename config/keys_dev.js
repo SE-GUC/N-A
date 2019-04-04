7cca98703c063cace68430c42b438e0084d4e3c2
amrabdelnabi1 <amrabdelnabi98@gmail.com>
2019-04-04 02:57:54 +0200

    Sprint 2 updated

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,2 +1,2 @@
-module.exports = {mongoURI: 'mongodb+srv://Mahmoudselim5:551912E4x5@cluster0-mr67e.mongodb.net/test?retryWrites=true'}
+module.exports = {mongoURI: 'mongodb+srv://mahmoud:mahmoud@cluster0-o30ve.mongodb.net/test?retryWrites=true'}
 
