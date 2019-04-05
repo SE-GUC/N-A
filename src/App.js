33761e0d83f1dca612ecf92d27e3405a901485f1
MahmouddAhmed <mahmoud.souad@student.guc.edu.eg>
2019-04-05 20:16:05 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import All_Projects from './components/All_Projects'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello User</h1>
-        <All_Projects/>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" render={props =>(
+            <div>
+            <h1>hello</h1>
+            <Link to="/Projects">All Projects</Link>
+            </div>
+            
+          )}/>       
+          <Route exact path="/Projects" component={All_Projects} />
+        </div>
+      </Router>
      
     );
   }
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types'
 
  class Project extends Component {
   
+  getStyle=()=>{
+    return {
+      background:'#E0FFFF',
+      
+    }
+  }
+
   render() {
     return (//P is Actullay the Project Info itself
-      <div>
+      <div style={this.getStyle()}>
         
         <h3>{this.props.P.name}</h3>
-        <p>{this.props.P.descreption}</p>
+        <h3>{this.props.P.descreption}</h3>
       </div>
     )
   }
