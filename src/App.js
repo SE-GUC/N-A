48e7aa6f4cacd3ff70f1ac1033a69b5e35e00eb3
unknown <seifmohamed728@gmail.com>
2019-04-07 14:09:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
+
 import './App.css';
+
 import All_Locations from './components/All_Locations'
+
 import All_Projects from './components/All_Projects'
+
 import Project_Requests from './components/Project_Requests'
+
 import { Route, BrowserRouter as Router  } from 'react-router-dom'
+
 import 'tachyons'
+import All_Partners from './components/All_Partners';
+
+
 
 class App extends React.Component {
+
   render() {
+
     return (
 
+
+
       <Router>
+
           <div>
+
           <Route exact path="/Locations" component={All_Locations}/>
+
           <Route exact path="/Projects" component={All_Projects}/>
+
           <Route exact path="/Project_Requests" component={Project_Requests}/>
+
+          <Route exact path="/All_Partners" component={All_Partners}/>
+
           </div>
+
       </Router>
+
     )}
+
 }
+
+
 
 export default App;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -11,6 +11,7 @@ class Header extends Component{
   <h2 className="mt2 mb0 f6 fw4 ttu tracked">Your amazing subtitle</h2>
   <nav className="bt bb tc mw7 center mt4">
     <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/Projects">Projects</a>
+    <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/Partners">Partners</a>
     <a className="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l" href="/portfolio">Portfolio</a>
     <a className="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" href="/shop">Shop</a>
     <a className="f6 f5-l link bg-animate black-80 hover-bg-light-pink dib pa3 ph4-l" href="/about">About</a>
