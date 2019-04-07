7c0f68416609dca20f1a1447f4e740236a7c4c49
YoussefH1998 <yh_fouad@hotmail.com>
2019-04-08 00:01:43 +0200

    sprint4

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
 import React from 'react';
+
 import './App.css';
+
 import All_Locations from './components/All_Locations'
+
 import All_Admins from './components/All_Admins'
-import Create_Admin from './components/Create_Admin'
 import All_Candidates from './components/All_Candidates'
+
 import All_Projects from './components/All_Projects'
+
 import Project_Requests from './components/Project_Requests'
+
+import All_co_working_spaces from './components/All_co_working_spaces'
+
+
 import { Route, BrowserRouter as Router  } from 'react-router-dom'
+
 import 'tachyons'
+import All_Partners from './components/All_Partners';
+
+
 
 class App extends React.Component {
+
   render() {
+
     return (
 
+
+
       <Router>
+
           <div>
+
           <Route exact path="/Locations" component={All_Locations}/>
+
+
+
+
           <Route exact path="/Admins" component={All_Admins}/>
           <Route exact path="/Candidates" component={All_Candidates}/>
+
           <Route exact path="/Projects" component={All_Projects}/>
+
           <Route exact path="/Project_Requests" component={Project_Requests}/>
-          <Route exact path="/CAdmins" component={Create_Admin}/>
+          <Route exact path="/co_working_spaces" component={All_co_working_spaces}/>
+
+
+          <Route exact path="/All_Partners" component={All_Partners}/>
+
           </div>
+
       </Router>
+
     )}
+
 }
+
+
 
 export default App;
